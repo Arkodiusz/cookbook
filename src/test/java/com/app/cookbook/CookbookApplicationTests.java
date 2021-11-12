ab1b934b42ffd8c951e987ab4bb71c11e32cf105
Arkodiusz <arecki358@gmail.com>
2021-11-12 15:10:41 +0100

    @autowired injections tests

--- src/test/java/com/app/cookbook/CookbookApplicationTests.java
+++ src/test/java/com/app/cookbook/CookbookApplicationTests.java
@@ -1,13 +1,32 @@
 package com.app.cookbook;
 
+import com.app.cookbook.controller.IngredientController;
+import com.app.cookbook.controller.RecipeController;
+import com.app.cookbook.service.IngredientService;
+import com.app.cookbook.service.RecipeService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class CookbookApplicationTests {
 
+	@Autowired
+	private RecipeController recipeController;
+	@Autowired
+	private RecipeService recipeService;
+	@Autowired
+	private IngredientController ingredientController;
+	@Autowired
+	private IngredientService ingredientService;
+
 	@Test
 	void contextLoads() {
+		assertNotNull(recipeController);
+		assertNotNull(recipeService);
+		assertNotNull(ingredientController);
+		assertNotNull(ingredientService);
 	}
-
 }
