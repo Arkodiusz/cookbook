6ebadda734657f8da5ac1c6e116bcf2164c59285
Arkodiusz <arecki358@gmail.com>
2021-11-17 09:07:32 +0100

    gh pages added to CORS mappings

--- src/main/java/com/app/cookbook/configuration/WebConfig.java
+++ src/main/java/com/app/cookbook/configuration/WebConfig.java
@@ -2,9 +2,7 @@ package com.app.cookbook.configuration;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
@@ -12,10 +10,8 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("http://localhost:3000", "https://arkodiusz.github.io")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
-//                .allowedHeaders("header1", "header2", "header3")
-//                .exposedHeaders("header1", "header2")
                 .allowCredentials(false).maxAge(3600);
     }
 
